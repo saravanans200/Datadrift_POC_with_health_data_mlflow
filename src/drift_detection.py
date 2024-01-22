ce8780f68b4598e38e333faa21dc3488ed8ab7c5
saravanans200 <47622302+saravanans200@users.noreply.github.com>
2024-01-22 10:47:37 +0530

    Update drift_detection.py

--- src/drift_detection.py
+++ src/drift_detection.py
@@ -128,7 +128,7 @@ if updated_dataset.shape[0] != insight['no._of_rows'][0] or updated_dataset.shap
             # Send the email and store the response
             #email_response = send_email(subject, content, i, attachments)  #commenting email function. test is working
             # Print the status of the email sending process
-            print(email_response)
+            #print(email_response)
             print("Mail sent to " + i)
 
 
